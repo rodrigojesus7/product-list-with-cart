cb0539b4109c58ff69a5dab726763efdec2cdd49
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-09-09 21:53:39 -0300

    feat: add basic html/js structure

---
+++ src/js/index.js
@@ -0,0 +1,56 @@
+const foodsContainer = document.querySelector('.foodsContainer')
+
+async function getData() {
+
+    try {
+        let response = await fetch('./data.json')
+        let data = await response.json()
+
+        console.log(data[0].image.thumbnail)
+        console.log(data)
+
+
+        foodsContainer.innerHTML = `${data.map(food => `
+        <div class="food">
+            <div>
+                <img src="${food.image.thumbnail}" alt="${food.name}">
+                <button class="addToCartBtn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20">
+                        <g fill="#C73B0F" clip-path="url(#a)">
+                            <path
+                                d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z" />
+                            <path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z" />
+                        </g>
+                        <defs>
+                            <clipPath id="a">
+                                <path fill="#fff" d="M.333 0h20v20h-20z" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+
+                    <p class="addToCartBtn__text">Add to Cart</p>
+                </button>
+            </div>
+
+            <div>
+                <h2> ${food.category}</h2>
+                <h3>${food.name}</h3>
+                <p>$<span>${food.price.toFixed(2)}</span></p>
+            </div>
+
+        </div>
+        `).join('')}`
+    }
+
+    catch (error) {
+        console.error('Erro ao carregar os dados:', error)
+    }
+}
+
+
+getData()
+
+
+
+
+
