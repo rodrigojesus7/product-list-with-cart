3a433256b5507b8cd46fac9e0da639347e706a53
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-09-11 21:57:19 -0300

    feat: add styled cart with quantity containar and buttons

--- src/js/index.js
+++ src/js/index.js
@@ -30,7 +30,7 @@ async function getData() {
             <div class="food__imageContainer">
                 <img class="food__image" src="${food.image[screenSize]}" alt="${food.name}">
                 
-                <button class="addToCartBtn">
+                <button class="addToCartBtn hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20">
                         <g fill="#C73B0F" clip-path="url(#a)">
                             <path
@@ -46,6 +46,22 @@ async function getData() {
 
                     <p class="addToCartBtn__text">Add to Cart</p>
                 </button>
+
+                <div class="addToCartWithQuantityContainer">
+                    <button class="addToCartWithQuantity__button addToCartWithQuantity__decrementButton">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2">
+                            <path fill="#fff" d="M0 .375h10v1.25H0V.375Z" />
+                        </svg>
+                    </button>
+
+                    <p class="addToCartWithQuantityContainer__text">0</p>
+
+                    <button class="addToCartWithQuantity__button addToCartWithQuantity__incrementButton">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+                            <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z" />
+                        </svg>
+                    </button>
+                 </div>
             </div>
 
             <div class="food_infoContainer">
